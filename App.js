9190b996bc700459e21e6b224a400cab735b2e1d
Pedro <93168925+pedro-patriota@users.noreply.github.com>
2023-02-26 09:43:02 -0300

    moving files to head

---
+++ App.js
@@ -0,0 +1,10 @@
+import {  } from "@reac";
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.js ato start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
